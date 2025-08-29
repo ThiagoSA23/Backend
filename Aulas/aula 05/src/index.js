8b10d372d8779825a0a6ab6b5e6672036173401d
Thiago Silva Azevedo <thiago.azevedo@iesb.edu.br>
2025-08-29 10:40:23 -0300

    testes de calculadora com o JEST

---
+++ Aulas/aula 05/src/index.js
@@ -0,0 +1,18 @@
+function soma(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function multi(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    if (b === 0) throw Error("Divisão por ZERO");
+    return a / b;
+}
+
+module.exports = { soma, sub, multi, div };
