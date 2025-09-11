58321f4ffcd1bfc1ce48059a661c076e2a155f84
Thiago Silva Azevedo <thiago.azevedo@iesb.edu.br>
2025-09-11 10:44:37 -0300

    codigos da aula 06 e 07

---
+++ Aulas/aula06/router.js
@@ -0,0 +1,28 @@
+// 1. Importar o framework
+const express = require("express");
+
+// middleware do roteamento
+const router = express.Router();
+
+router.get('/', (req, res) =>{
+    res.send("Liatar as tarefas");
+});
+
+router.post('/', (req, res) =>{
+    console.log(req.body);
+    res.status(201).send("Tarefa criada com sucesso");
+});
+
+router.put('/:id', (req, res) =>{
+    const { id } = req.params; // desestruturando o objeto params 
+    if (id == 1) return res.send("Tarefa atualizada");
+    res.status(404).send("Tarefa não encontrada");
+});
+
+router.delete('/:id', (req, res) =>{
+    const { id } = req.params; // desestruturando o objeto params 
+    if (id == 1) return res.status(204).end();
+    throw Error("Tarefa não encontrada");
+});
+
+module.exports = router;
